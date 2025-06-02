6b3b418a424615caed87ab330b4d147b28bd2f4e
Fatma Abdelhaleem <fatmaabdelhaleem22@gmail.com>
2025-06-02 14:18:24 +0300

    print statement

---
+++ Task1.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+     // program1
+     // print this
+     //***********************
+     // Name: Fatma Abdelhaleem
+     // Age: 23
+     // City: Zefta
+     // Country: Egypt
+     // ***********************
+
+     cout << "***********************\n";
+     cout << "Name: Fatma Abdelhaleem\n";
+     cout << "Age: 23\n";
+     cout << "City: Zefta\n";
+     cout << "Country: Egypt\n";
+     cout << "***********************\n";
+
+     // program2
+     // print this
+     // ********
+     // ********
+     // ********
+     // ********
+
+     cout << "********\n";
+     cout << "********\n";
+     cout << "********\n";
+     cout << "********\n";
+     cout << "***********************\n";
+
+     // program3
+     // print this
+     // ***********************
+     // I love programming!
+
+     // I promise to be the best developer ever!
+
+     // I know it will take time to practice,but I promised my self to acheive my goal
+
+     // best regards,
+     // Fatma Abdelhaleem
+
+     cout << "I love programming!\n"
+          << endl;
+     cout << "I promise to be the best developer ever!\n"
+          << endl;
+     cout << "I know it will take time to practice,but I promised my self to acheive my goal\n"
+          << endl;
+     cout << "best regards,\n";
+     cout << "Fatma Abdelhaleem\n\n";
+
+     // program3
+     // print this
+     // *   *
+     // *   *
+     // *****
+     // *   *
+     // *   *
+     cout << "*   *\n";
+     cout << "*   *\n";
+     cout << "*****\n";
+     cout << "*   *\n";
+     cout << "*   *\n";
+     return 0;
+}
