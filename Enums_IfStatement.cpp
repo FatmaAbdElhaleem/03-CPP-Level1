ef6db0ce3791fbe5b7f6aba2bda7b61ec8d564f9
Fatma Abd Elhaleem <fatmaabdelhaleem22@gmail.com>
2025-08-26 12:10:54 +0300

    using switch case

--- Enums_IfStatement.cpp
+++ Enums_IfStatement.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-//write this program using enums & else if:
+//write this program using enums & else if once and switch another time :
 // ****************************
 // Please Enter the number of your country?
 // (1) Jordan
@@ -17,25 +17,56 @@ using namespace std;
 
 enum enCountries{Jordan=1,Tunisa=2,Algeria=3,Oman=4,Egypt=5,Iraq=6,Other=7};
 
+//solution using else if 
+// void checkCountry(enCountries country){
+//    if(country==enCountries::Jordan){
+//     cout<<"Jordon";
+//    }else if(country==enCountries::Tunisa){
+//     cout<<"Tunisa";
+//    }else if(country==enCountries::Algeria){
+//     cout<<"Algeria";
+//    }else if(country==enCountries::Oman){
+//     cout<<"Oman";
+//    }else if(country==enCountries::Egypt){
+//     cout<<"Egypt";
+//    }else if(country==enCountries::Iraq){
+//     cout<<"Iraq";
+//    }else if(country==enCountries::Other){
+//     cout<<"Other";
+//    }else{
+//     cout<<"Wrong Answer";
+//    }
+// }
+
+//solution with switch :
 void checkCountry(enCountries country){
-   if(country==enCountries::Jordan){
-    cout<<"Jordon";
-   }else if(country==enCountries::Tunisa){
-    cout<<"Tunisa";
-   }else if(country==enCountries::Algeria){
-    cout<<"Algeria";
-   }else if(country==enCountries::Oman){
-    cout<<"Oman";
-   }else if(country==enCountries::Egypt){
-    cout<<"Egypt";
-   }else if(country==enCountries::Iraq){
-    cout<<"Iraq";
-   }else if(country==enCountries::Other){
-    cout<<"Other";
-   }else{
-    cout<<"Wrong Answer";
-   }
+    switch (country){
+     case enCountries::Jordan:
+         cout<<"Jordan";
+         break;
+     case enCountries::Tunisa:
+         cout<<"Tunisa";
+         break;
+     case enCountries::Algeria:
+         cout<<"Algeria";
+         break;
+     case enCountries::Oman:
+         cout<<"Oman";
+         break;
+     case enCountries::Egypt:
+         cout<<"Egypt";
+         break;
+     case enCountries::Iraq :
+         cout<<"Iraq";
+         break;
+     case enCountries::Other :
+         cout<<"Other";
+         break;
+     default:
+       cout<<"Wrong Answer";
+    }
 }
+
 
 int main(){
     cout << "****************************\n"; 
